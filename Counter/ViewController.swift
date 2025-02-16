837365c0db237a500296a479bb75320f708125e1
damir345 <saldam@inbox.ru>
2025-02-16 20:08:20 +0300

    working

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private var timesPressed: Int = 0
 
+    @IBOutlet weak var buttonAdd: UIButton!
+    
+    @IBOutlet weak var buttonRem: UIButton!
+    
+    @IBOutlet weak var counter: UILabel!
+    
+    @IBOutlet weak var changesHistory: UITextView!
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        buttonAdd.setTitle("+", for: .normal)
+        buttonRem.setTitle("-", for: .normal)
+        counter.text = "\(timesPressed)"
+        
+        
+        // buttonTouch.setTitle("highlited", for: .highlighted )
     }
-
-
+    @IBAction func buttonAdd(_ sender: Any) {
+        timesPressed += 1
+        counter.text = "Значение счётчика: \(timesPressed)"
+    }
+    @IBAction func buttonRem(_ sender: UIButton) {
+        timesPressed -= 1
+        timesPressed = max(timesPressed, 0)
+        counter.text = "Значение счётчика: \(timesPressed)"
+    }
+    
+    
 }
 
